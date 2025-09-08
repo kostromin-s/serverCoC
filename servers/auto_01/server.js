79e1fc4ae9ac7c3138b4353f58400a573700fe70
kostromin-s <namly6702@gmail.com>
2025-09-08 13:29:48 +0700

    dayPoint,warPoint,pingServer

--- servers/auto_01/server.js
+++ servers/auto_01/server.js
@@ -1,13 +1,22 @@
+import express from "express";
 import schedule from "node-schedule";
 import dotenv from "dotenv";
 dotenv.config();
 
 import connectDB from "./config/connectDB.js";
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server auto_01 is running!");
+});
+
 (async () => {
   await connectDB();
   // Import cron job sau khi DB đã kết nối
   await import("./controllers/cronJob.js");
 
-  console.log("🚀 Server auto_01 started.");
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("🚀 Server auto_01 started.");
+  });
 })();
